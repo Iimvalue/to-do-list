563d083e3ffdd26d96fcbe88765a979f6d6c18c4
Abdulrahman <1xvlux1@gmail.com>
2025-01-20 19:20:12 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import {
   Divider,
   IconButton,
   Button,
+  Grid,
 } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Card from "@mui/material/Card";
@@ -21,247 +21,282 @@ import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
-let counterId = 1;
+let counterCardId = 1;
+export default function CleanCode() {
+  const [tasksData, setTasksData] = useState([]);
+  const [tasksDataDisplay, setTasksDataDisplay] = useState([]);
+  const [editTaskData, setEditTaskData] = useState({});
+  const [addNewTask, setAddNewTask] = useState();
 
-function App() {
-  const [showTask, setShowTask] = useState([]);
-  const [allTasks, setAllTasks] = useState([]);
-
-  const [addNewTask, setAddNewTask] = useState("");
-
-  // Toggle button variables and handles
-  const [alignment, setAlignment] = useState("all");
-  const handleChange = (event, newAlignment) => {
-    if (newAlignment !== null) {
-      setAlignment(newAlignment);
+  // Toggle button variables and handlers
+  const [currentToggleSelection, setCurrentToggleSelection] = useState("all");
+  const handleToggleChange = (event, toggleSelected) => {
+    if (toggleSelected !== null) {
+      setCurrentToggleSelection(toggleSelected);
     }
   };
 
-  // Dialog variables and handles
-  const [open, setOpen] = useState(false);
-  const [tempTask, setTempTask] = useState({});
-  function handleOpen(id) {
-    let task = allTasks.filter((task) => task.id === id);
-    setTempTask(task[0]);
-    setOpen(true);
+  // Snackbar variables and handlers
+  const [isSnackBarOpen, setIsSnackBarOpen] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("");
+  const [snackBarColor, setSnackBarColor] = useState("");
+  function handleSnackBarOpen() {
+    setIsSnackBarOpen(true);
   }
-  const handleClose = () => {
-    setOpen(false);
+  function handleSnackBarClose() {
+    setIsSnackBarOpen(false);
+  }
+
+  // Edition Dialog variables and handles
+  const [isDialogEditOpen, setIsDialogEditOpen] = useState(false);
+  function handleDialogEditOpen() {
+    setIsDialogEditOpen(true);
+  }
+  const handleDialogEditClose = () => {
+    setIsDialogEditOpen(false);
   };
 
-  // Handlers
-  function deletedHandler(id) {
-    let tasks = showTask.filter((item) => {
+  // Deletion dialogs variables and handles
+  const [isDialogDeleteOpen, setIsDialogDeleteOpen] = useState(false);
+  const [deleteTaskId, setDeleteTaskId] = useState();
+  const handleDialogDeleteOpen = () => {
+    setIsDialogDeleteOpen(true);
+  };
+  const handleDialogDeleteClose = () => {
+    setIsDialogDeleteOpen(false);
+  };
+
+  // Handlers Tasks
+  function deleteTaskHandler(id) {
+    let tasks = tasksData.filter((item) => {
       return item.id !== id;
     });
-    
-    setAllTasks(tasks);
-    setShowTask(tasks);
+
+    return tasks;
   }
 
-  function completedHandler(id) {
-    let tasks = allTasks.map((task) => {
+  function completeTaskHandler(id) {
+    let tasks = tasksData.map((task) => {
       if (task.id === id) {
         return { ...task, isCompleted: true };
       }
       return task;
     });
 
-    setAllTasks(() => tasks);
-    if (alignment === "uncompleted") {
-      setShowTask(() =>
-        tasks.filter((obj) => {
-          return !obj.isCompleted;
-        })
-      );
-    }
+    return tasks;
   }
 
-  function editHandler(id) {
-    let tasks = allTasks.map((task) => {
+  function editTaskHandler(id) {
+    let tasks = tasksData.map((task) => {
       if (task.id === id) {
         return {
           ...task,
-          title: tempTask.title,
-          description: tempTask.description,
+          title: editTaskData.title,
+          description: editTaskData.description,
         };
       }
       return task;
     });
 
-    setAllTasks(() => tasks);
-    if (alignment === "completed") {
-      setShowTask(() =>
+    return tasks;
+  }
+
+  function updateTasks(tasks) {
+    setTasksData(() => tasks);
+
+    if (currentToggleSelection === "completed") {
+      setTasksDataDisplay(() =>
         tasks.filter((task) => {
           return task.isCompleted;
         })
       );
-    } else if (alignment === "uncompleted") {
-      setShowTask(() =>
+    } else if (currentToggleSelection === "uncompleted") {
+      setTasksDataDisplay(() =>
         tasks.filter((task) => {
           return !task.isCompleted;
         })
       );
     } else {
-      setShowTask(() => tasks);
+      setTasksDataDisplay(() => tasks);
     }
   }
 
   return (
-    <Container
-      maxWidth={"xs"}
-      sx={{
-        position: "absolute",
-        backgroundColor: "white",
-        top: { xs: "10%" },
-        left: { xs: "20%", md: "35%" },
-        borderRadius: 3,
-      }}
-    >
-      <Grid
-        container
-        rowSpacing={2}
-        justifyContent={"center"}
-        textAlign={"center"}
+    <>
+      <Container
+        maxWidth={"xs"}
+        sx={{
+          position: "absolute",
+          backgroundColor: "white",
+          top: { xs: "10%" },
+          left: { xs: "20%", md: "35%" },
+          borderRadius: 3,
+        }}
       >
-        <Grid item xs={12}>
-          <Typography variant="h2" color="initial">
-            مهامي
-          </Typography>
-          <Divider />
-        </Grid>
+        <Grid
+          container
+          rowSpacing={2}
+          justifyContent={"center"}
+          textAlign={"center"}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h2" color="initial">
+              مهامي
+            </Typography>
+            <Divider />
+          </Grid>
 
-        <Grid item xs={12}>
-          <ToggleButtonGroup
-            color="error"
-            value={alignment}
-            exclusive
-            onChange={handleChange}
-            aria-label="Platform"
-          >
-            <ToggleButton
-              value={"uncompleted"}
-              onClick={() => {
-                setShowTask(
-                  allTasks.filter((obj) => {
-                    return !obj.isCompleted;
-                  })
-                );
-              }}
+          <Grid item xs={12}>
+            <ToggleButtonGroup
+              color="error"
+              value={currentToggleSelection}
+              exclusive
+              onChange={handleToggleChange}
+              aria-label="Platform"
             >
-              غير منجز
-            </ToggleButton>
-            <ToggleButton
-              value="completed"
-              onClick={() => {
-                setShowTask(
-                  allTasks.filter((obj) => {
-                    return obj.isCompleted;
-                  })
-                );
-              }}
-            >
-              منجز
-            </ToggleButton>
-            <ToggleButton
-              value="all"
-              onClick={() => {
-                setShowTask(() => allTasks);
-              }}
-            >
-              الكل
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </Grid>
-
-        <Grid item xs={12}>
-          {showTask.map((task) => (
-            <Box key={task.id}>
-              <TaskDetails
-                id={task.id}
-                title={task.title}
-                description={task.description}
-                onClickDelete={deletedHandler}
-                onCompleteClick={completedHandler}
-                onOpen={handleOpen}
-              />
-            </Box>
-          ))}
-        </Grid>
-        <Grid item xs={12}>
-          <FormDialogForEdit
-            task={tempTask}
-            setHandler={setTempTask}
-            openDialog={open}
-            onClose={handleClose}
-            onEditClick={editHandler}
-          />
-        </Grid>
-
-        <Grid item xs={12}>
-          <Grid container justifyContent={"flex-end"}>
-            <Grid item xs sx={{ marginBottom: "10px" }}>
-              <TextField
-                id="outlined-basic"
-                fullWidth
-                label="عنوان المهمة"
-                variant="outlined"
-                value={addNewTask}
-                onChange={(e) => {
-                  setAddNewTask(e.target.value);
-                }}
-                sx={{ mb: 1 }}
-              />
-              <Button
+              <ToggleButton
+                value={"uncompleted"}
                 onClick={() => {
-                  let allAndNewTask = [
-                    ...allTasks,
-                    {
-                      id: counterId,
-                      title: addNewTask,
-                      description: "",
-                      isCompleted: false,
-                    },
-                  ];
-
-                  setAllTasks(() => allAndNewTask);
-                  if (alignment === "uncompleted") {
-                    setShowTask(() =>
-                      allAndNewTask.filter((obj) => {
-                        return !obj.isCompleted;
-                      })
-                    );
-                  } else if (alignment === "completed") {
-                    setShowTask(() =>
-                      allAndNewTask.filter((obj) => {
-                        return obj.isCompleted;
-                      })
-                    );
-                  } else {
-                    setShowTask(() => allAndNewTask);
-                  }
-                  setAddNewTask(() => "");
-                  counterId += 1;
+                  setTasksDataDisplay(
+                    tasksData.filter((obj) => {
+                      return !obj.isCompleted;
+                    })
+                  );
                 }}
-                variant="contained"
-                color="error"
-                sx={{ width: "35%", fontSize: "20px" }}
               >
-                إضافة
-              </Button>
+                غير منجز
+              </ToggleButton>
+              <ToggleButton
+                value="completed"
+                onClick={() => {
+                  setTasksDataDisplay(
+                    tasksData.filter((obj) => {
+                      return obj.isCompleted;
+                    })
+                  );
+                }}
+              >
+                منجز
+              </ToggleButton>
+              <ToggleButton
+                value="all"
+                onClick={() => {
+                  setTasksDataDisplay(() => tasksData);
+                }}
+              >
+                الكل
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
+
+          <Grid item xs={12}>
+            {tasksDataDisplay.map((task) => (
+              <Box key={task.id}>
+                <DisplayTaskAsCard
+                  task={task}
+                  handleSnackBarOpen={handleSnackBarOpen}
+                  handleDialogEditOpen={handleDialogEditOpen}
+                  updateTasks={updateTasks}
+                  onCompleteClick={completeTaskHandler}
+                  setEditTaskData={setEditTaskData}
+                  onDeleteClick={deleteTaskHandler}
+                  handleDialogDeleteOpen={handleDialogDeleteOpen}
+                  setDeleteTaskId={setDeleteTaskId}
+                  setSnackBarMessage={setSnackBarMessage}
+                  setSnackBarColor={setSnackBarColor}
+                />
+              </Box>
+            ))}
+          </Grid>
+
+          <Grid item xs={12}>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item xs sx={{ marginBottom: "10px" }}>
+                <TextField
+                  id="outlined-basic"
+                  fullWidth
+                  label="عنوان المهمة"
+                  variant="outlined"
+                  value={addNewTask}
+                  onChange={(e) => {
+                    setAddNewTask(e.target.value);
+                  }}
+                  sx={{ mb: 1 }}
+                />
+                <Button
+                  onClick={() => {
+                    let tasksDataWithNewTask = [
+                      ...tasksData,
+                      {
+                        id: counterCardId,
+                        title: addNewTask,
+                        description: "",
+                        isCompleted: false,
+                      },
+                    ];
+
+                    updateTasks(tasksDataWithNewTask);
+
+                    setAddNewTask("");
+                    counterCardId += 1;
+                  }}
+                  variant="contained"
+                  color="error"
+                  sx={{ width: "35%", fontSize: "20px" }}
+                >
+                  إضافة
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
+
+          <Grid item xs={12}>
+            <DialogEditForm
+              isDialogOpen={isDialogEditOpen}
+              task={editTaskData}
+              setEditTaskData={setEditTaskData}
+              onEditClick={editTaskHandler}
+              updateTasks={updateTasks}
+              handleDialogClose={handleDialogEditClose}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <DialogDeleteForm
+              isDialogOpen={isDialogDeleteOpen}
+              taskId={deleteTaskId}
+              onDeleteClick={deleteTaskHandler}
+              updateTasks={updateTasks}
+              handleDialogClose={handleDialogDeleteClose}
+              setSnackBarMessage={setSnackBarMessage}
+              setSnackBarColor={setSnackBarColor}
+              handleSnackBarOpen={handleSnackBarOpen}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            {/*  */}
+          </Grid>
+
+          <SnackBar
+            message={snackBarMessage}
+            color={snackBarColor}
+            isSnackBarOpen={isSnackBarOpen}
+            handleSnackBarClose={handleSnackBarClose}
+            handleSnackBarOpen={handleSnackBarOpen}
+          />
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 }
 
-export default App;
-
-function TaskDetails(props) {
+function DisplayTaskAsCard(props) {
   return (
     <Box>
       <Card
@@ -280,23 +315,32 @@ function TaskDetails(props) {
                 color="white"
                 sx={{ wordWrap: "break-word" }}
               >
-                {props.title}
+                {props.task.title}
               </Typography>
               <Typography variant="subtitle2" color="gray">
-                {props.description}
+                {props.task.description}
               </Typography>
             </CardContent>
           </Grid>
 
           <Grid item xs={6}>
             <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
-              <IconButton onClick={() => props.onCompleteClick(props.id)}>
+              <IconButton
+                onClick={() => {
+                  let tasks = props.onCompleteClick(props.task.id);
+                  props.updateTasks(tasks);
+                  props.handleSnackBarOpen();
+                  props.setSnackBarMessage("تم نقل المهمة الى منجز بنجاح");
+                  props.setSnackBarColor("success");
+                }}
+              >
                 <CheckCircleIcon sx={{ color: "white" }} />
               </IconButton>
 
               <IconButton
                 onClick={() => {
-                  props.onOpen(props.id);
+                  props.setEditTaskData(props.task);
+                  props.handleDialogEditOpen();
                 }}
               >
                 <EditIcon sx={{ color: "white" }} />
@@ -304,7 +348,13 @@ function TaskDetails(props) {
 
               <IconButton
                 onClick={() => {
-                  props.onClickDelete(props.id);
+                  props.handleDialogDeleteOpen();
+                  props.setDeleteTaskId(props.task.id);
+                  // if (props.isDeleteAlert) {
+                  //   let tasks = props.onDeleteClick(props.task.id);
+                  //   props.updateTasks(tasks);
+                  //   props.setIsDeleteAlert(false);
+                  // }
                 }}
               >
                 <DeleteOutlineIcon sx={{ color: "white" }} />
@@ -317,13 +367,12 @@ function TaskDetails(props) {
   );
 }
 
-function FormDialogForEdit(props) {
-
+function DialogEditForm(props) {
   return (
     <Dialog
       sx={{ direction: "rtl" }}
-      open={props.openDialog}
-      onClose={props.onClose}
+      open={props.isDialogOpen}
+      onClose={props.handleDialogClose}
     >
       <DialogTitle>تعديل المهمة</DialogTitle>
       <DialogContent sx={{ direction: "rtl" }}>
@@ -337,7 +386,7 @@ function FormDialogForEdit(props) {
           variant="standard"
           value={props.task.title}
           onChange={(e) => {
-            props.setHandler({ ...props.task, title: e.target.value });
+            props.setEditTaskData({ ...props.task, title: e.target.value });
           }}
         />
 
@@ -351,28 +400,86 @@ function FormDialogForEdit(props) {
           variant="standard"
           value={props.task.description}
           onChange={(e) => {
-            props.setHandler({ ...props.task, description: e.target.value });
+            props.setEditTaskData({
+              ...props.task,
+              description: e.target.value,
+            });
           }}
         />
       </DialogContent>
       <DialogActions>
         <Button
           onClick={() => {
-            props.onClose();
+            props.handleDialogClose();
           }}
         >
           إلغاء
         </Button>
         <Button
           onClick={() => {
-            console.log(props.task);
-            props.onEditClick(props.task.id);
-            props.onClose();
+            let tasks = props.onEditClick(props.task.id);
+            props.updateTasks(tasks);
+            props.handleDialogClose();
           }}
         >
           تعديل
         </Button>
       </DialogActions>
     </Dialog>
+  );
+}
+
+function DialogDeleteForm(props) {
+  return (
+    <Dialog
+      open={props.isDialogOpen}
+      onClose={props.handleDialogClose}
+      dir="rtl"
+    >
+      <DialogTitle>{"هل أنت متاكد برغبتك في حذف هذه المهمة ؟"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          لا يمكنك التراجع في حال اختيار زر حذف
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => props.handleDialogClose()}>إلغاء</Button>
+        <Button
+          color="error"
+          onClick={() => {
+            let tasks = props.onDeleteClick(props.taskId);
+            props.updateTasks(tasks);
+            props.handleDialogClose();
+            props.setSnackBarMessage("تم حذف المهمة بنجاح");
+            props.setSnackBarColor("success");
+            props.handleSnackBarOpen();
+            
+          }}
+          autoFocus
+        >
+          حذف
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+function SnackBar(props) {
+  return (
+    <Snackbar
+      open={props.isSnackBarOpen}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      autoHideDuration={3000}
+      onClose={props.handleSnackBarClose}
+    >
+      <Alert
+        onClose={props.handleSnackBarClose}
+        severity={props.color}
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        {props.message}
+      </Alert>
+    </Snackbar>
   );
 }
